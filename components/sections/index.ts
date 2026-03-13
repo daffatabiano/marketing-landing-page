338ac86a4b0bc4ac0142ad1755f56f036a3ccc3e
daffatabiano <daffatabianoo@gmail.com>
2026-03-14 00:30:57 +0700

    feat: Add sections index file to export various components

---
+++ components/sections/index.ts
@@ -0,0 +1,8 @@
+// export { HeroSection } from "./HeroSection";
+export { FeaturesSection } from "./FeatureSection";
+export { StatsSection } from "./StatsSection";
+export { HowItWorksSection } from "./HowitworksSection";
+export { PricingSection } from "./PricingSection";
+export { TestimonialsSection } from "./TestimonialsSection";
+export { FAQSection } from "./FaqSection";
+export { CTASection } from "./CtaSection";
